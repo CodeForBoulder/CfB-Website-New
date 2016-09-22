2a97b63449ed41450dc1bc0a7e8b984e93a0f560
Alex Finnarn <alexfinnarn@gmail.com>
2016-09-21 19:48:23 -0600

    intial commit for Github API integration

---
+++ js/custom/github-api.js
@@ -0,0 +1,10 @@
+/**
+ * Integration for the Github API.
+ * @file
+ *
+ * The idea would be for this class to return an object that can easily be
+ * rendered in a template file. The object would have properties for "projects"
+ * and "members" initially.
+ */
+
+
